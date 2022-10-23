f95c9ad42d5e9817d77c02311246899a552b970d
Alex Chan <alex@alexwlchan.net>
2022-10-23 20:10:08 +0100

    get some images in the gallery

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
+import { readFile } from 'fs';
+import { ItemView, Plugin, TFile, WorkspaceLeaf } from 'obsidian';
 
-import { ItemView, WorkspaceLeaf } from "obsidian";
+export const VIEW_TYPE = "awlc-gallery-view";
 
-export const VIEW_TYPE_EXAMPLE = "example-view";
-
-export class ExampleView extends ItemView {
+export class GalleryView extends ItemView {
   constructor(leaf: WorkspaceLeaf) {
     super(leaf);
   }
 
   getViewType() {
-    return VIEW_TYPE_EXAMPLE;
+    return VIEW_TYPE;
   }
 
   getDisplayText() {
@@ -24,169 +23,48 @@ export class ExampleView extends ItemView {
   async onOpen() {
     const container = this.containerEl.children[1];
     container.empty();
-    container.createEl("h4", { text: "Example view" });
-    container.createEl("p", { text: "This is some text" });
+
+    const vaultFiles: TFile[] = this.app.vault.getFiles();
+    const imageFiles = vaultFiles.filter(vaultFile =>
+      vaultFile.extension === 'png' || vaultFile.extension === 'jpeg' || vaultFile.extension === 'jpg'
+    );
+
+    for (const image of imageFiles) {
+      const imagePath = this.app.vault.adapter.getResourcePath(image.path)
+
+      const link = container.createEl("a", { text: image.path, attr: { href: image.path }});
+      container.createEl("img", { attr: { src: imagePath }, parent: link });
+    }
+
+    container.createEl("pre", { text: vaultFiles.toString() });
+    container.createEl("p", { text: "after the pre" });
   }
-
-  async onClose() {
-    // Nothing to clean up.
-  }
-}
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
     // see https://marcus.se.net/obsidian-plugin-docs/user-interface/views
-    this.registerView(
-      VIEW_TYPE_EXAMPLE,
-      (leaf) => new ExampleView(leaf)
-    );
+    this.registerView(VIEW_TYPE, (leaf) => new GalleryView(leaf));
 
-
-
-		// This creates an icon in the left ribbon.
 		this.addRibbonIcon('image-file', 'Open gallery', () => {
       this.activateView();
-			// Called when the user clicks the icon.
-      // new Notice('This is a notice!');
 		});
-		// Perform additional things with the ribbon
-    // ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    // const statusBarItemEl = this.addStatusBarItem();
-    // statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-    // this.addCommand({
-    //   id: 'open-sample-modal-simple',
-    //   name: 'Open sample modal (simple)',
-    //   callback: () => {
-    //     new SampleModal(this.app).open();
-    //   }
-    // });
-    // // This adds an editor command that can perform some operation on the current editor instance
-    // this.addCommand({
-    //   id: 'sample-editor-command',
-    //   name: 'Sample editor command',
-    //   editorCallback: (editor: Editor, view: MarkdownView) => {
-    //     console.log(editor.getSelection());
-    //     editor.replaceSelection('Sample Editor Command');
-    //   }
-    // });
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-    // this.addCommand({
-    //   id: 'open-sample-modal-complex',
-    //   name: 'Open sample modal (complex)',
-    //   checkCallback: (checking: boolean) => {
-    //     // Conditions to check
-    //     const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-    //     if (markdownView) {
-    //       // If checking is true, we're simply "checking" if the command can be run.
-    //       // If checking is false, then we want to actually perform the operation.
-    //       if (!checking) {
-    //         new SampleModal(this.app).open();
-    //       }
-    //
-    //       // This command will only show up in Command Palette when the check function returns true
-    //       return true;
-    //     }
-    //   }
-    // });
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-    // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-    //   console.log('click', evt);
-    // });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
   async onunload() {
-    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE);
   }
 
   async activateView() {
-    this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE);
 
     await this.app.workspace.getLeaf(false).setViewState({
-      type: VIEW_TYPE_EXAMPLE,
+      type: VIEW_TYPE,
       active: true,
     });
 
     this.app.workspace.revealLeaf(
-      this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+      this.app.workspace.getLeavesOfType(VIEW_TYPE)[0]
     );
   }
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
